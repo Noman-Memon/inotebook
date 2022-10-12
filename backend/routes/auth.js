eb491f8d455cdef5162de0fdfe8634519fa42c8f
ABASITJAMALBROTHERS <noman.rauf@jamalbrothers.com.pk>
2022-10-12 16:43:14 +0500

    create inotebook backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,37 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
 
 // create a user using : POST "api/auth/".
-router.post('/', (req, res) => {
-  console.log(req.body)
-  const user = User(req.body)
-  user.save()
-  res.send('Hey!')
+router.post(
+  '/',
+  [
+    body('name', 'Enter a valid name').isLength({ min: 3 }), // if we want to add any message with this type body('name','Enter a valid name').isLength({ min: 3 })
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err)
+        res.json({ error: 'Please enter a unique value' })
+      })
 
-  //   obj = {
-  //     a: 'hey!',
-  //     num: 35,
-  //   }
-  //   res.json(obj)
-})
+    //   obj = {
+    //     a: 'hey!',
+    //     num: 35,
+    //   }
+    //   res.json(obj)
+  },
+)
 module.exports = router
